e4605c8feffc4f48524e49fdb7f5616b8a10d254
julien382 <julhermain@gmail.com>
2022-08-23 14:26:17 +0200

    elementTri js appareil ustensile

--- scripts/app/handleAppareilInput.js
+++ scripts/app/handleAppareilInput.js
@@ -5,31 +5,36 @@ import { getLocalStorage } from "./utils/localStorage.js"
 import { filterRecipiesByAppareil } from "./utils/filterRecipiesByAppareil.js"
 
 export const handleInputAppareil = () => {
-  const inputContainer = document.querySelector('.elementTri2')
+  const inputContainer = document.querySelector('.elementTri__flex2')
   const arrow = inputContainer.querySelector('.elementTri__arrow')
   const dropdown = document.querySelector('.elementTri__dropdown2')
 
   inputContainer.addEventListener('click', () => {
 
     const input = inputContainer.querySelector('.elementTri__input')
-    /*input.focus()*/
+    input.focus()
 
-    input.addEventListener('focus', () => {
-      input.placeholder = 'Rechercher un appareil';
-    })
-
+    arrow.classList.toggle('elementTri__arrow--rotate')
+    dropdown.classList.toggle('elementTri__dropdown--active')
+    console.log(dropdown);
+    // !! bien penser à vider la dropdown à chaque fois !!
     const list = inputContainer.querySelector('.appareil-list')
+    list.innerHTML = '';
 
     // remplir la dropdown
     const allRecipies = getLocalStorage();
     const allAppareils = getAllAppareils(allRecipies); // ['blender', 'saladier', 'cocotte']
 
+    // supprimer les appareils qui sont dans les tags
+    // 1- récup tout le contenu (div.innerText) des tags appareils
+    // 2- retirer les appareils de allAppareils qu'on a enlever à l'etape 1
+    // allAppareils = ['pomme', 'poire', 'fraise', 'ananas', 'kiwi']
+    // tags = ['fraise', 'kiwi']
+    // appareils tout display allAppareils - tags
 
-    // !! bien penser à vider la dropdown à chaque fois !!
-    list.innerHTML = '';
-    
+    // allAppareils de la boucle (ligne 38) le modifier (le nom) par ce que j'ai créer étape 2
+   
     // boucle sur  allAppareils afin d'insérer les éléments dans la dropdown
-    // <li>*appareil*</li>
     allAppareils.forEach(appareil => {
       const liDropdown = document.createElement('li');      
       liDropdown.innerHTML = appareil;
@@ -37,22 +42,17 @@ export const handleInputAppareil = () => {
 
       liDropdown.addEventListener('click', () => {
         createTag(appareil, 'appareil')
-        
-        // [TODO]
-        // filterRecipiesByAppareil(appareil)
-        // const allRecipies = getLocalStorage();
-        // displayRecipes(allRecipies)
+
         filterRecipiesByAppareil(appareil)
         const allRecipiesAppareil = getLocalStorage();
         displayRecipies(allRecipiesAppareil)
+        //list.remove()
+        arrow.classList.toggle('elementTri__arrow--rotate')
+        dropdown.classList.toggle('elementTri__dropdown--active')
       })
-
     });
+
   })
 
-   // rotate de la flèche et ajout de la dropdown (avec les éléments li)
-   arrow.addEventListener('click', () => {
-    arrow.classList.toggle('elementTri__arrow--rotate')
-    dropdown.classList.toggle('elementTri__dropdown--active')
-  })
+
 }
--- scripts/app/handleUstensileInput.js
+++ scripts/app/handleUstensileInput.js
@@ -5,31 +5,36 @@ import { getLocalStorage } from "./utils/localStorage.js"
 import { filterRecipiesByUstensile } from "./utils/filterRecipiesByUstensile.js"
 
 export const handleInputUstensile = () => {
-  const inputContainer = document.querySelector('.elementTri3')
+  const inputContainer = document.querySelector('.elementTri__flex3')
   const arrow = inputContainer.querySelector('.elementTri__arrow')
   const dropdown = document.querySelector('.elementTri__dropdown3')
 
   inputContainer.addEventListener('click', () => {
 
     const input = inputContainer.querySelector('.elementTri__input')
-    /*input.focus()*/
+    input.focus()
 
-    input.addEventListener('focus', () => {
-      input.placeholder = 'Rechercher un ustensile';
-    })
-
+    arrow.classList.toggle('elementTri__arrow--rotate')
+    dropdown.classList.toggle('elementTri__dropdown--active')
+    console.log(dropdown);
+    // !! bien penser à vider la dropdown à chaque fois !!
     const list = inputContainer.querySelector('.ustensile-list')
+    list.innerHTML = '';
 
     // remplir la dropdown
     const allRecipies = getLocalStorage();
     const allUstensiles = getAllUstensiles(allRecipies); // ['passoire', 'verres', 'couteau']
 
+    // supprimer les ustensiles qui sont dans les tags
+    // 1- récp tout le contenu (div.innerText) des tags ustensiles
+    // 2- retirer les ustensiles de allUstensiles qu'on a enlever à l'etape 1
+    // allUstensiles = ['pomme', 'poire', 'fraise', 'ananas', 'kiwi']
+    // tags = ['fraise', 'kiwi']
+    // ustensile tout display allUstensiles - tags
 
-    // !! bien penser à vider la dropdown à chaque fois !!
-    list.innerHTML = '';
+    // allUstensiles de la boucle (ligne 38) le modifier (le nom) par ce que j'ai créer étape 2
     
-    // boucle sur  allIngredients afin d'insérer les éléments dans la dropdown
-    // <li>*ingredient*</li>
+    // boucle sur  allUstensiles afin d'insérer les éléments dans la dropdown
     allUstensiles.forEach(ustensile => {
       const liDropdown = document.createElement('li');      
       liDropdown.innerHTML = ustensile;
@@ -38,21 +43,16 @@ export const handleInputUstensile = () => {
       liDropdown.addEventListener('click', () => {
         createTag(ustensile, 'ustensile')
         
-        // [TODO]
-        // filterRecipiesByUstensile(ustensile)
-        // const allRecipies = getLocalStorage();
-        // displayRecipes(allRecipies)
         filterRecipiesByUstensile(ustensile)
         const allRecipiesUstensile = getLocalStorage();
         displayRecipies(allRecipiesUstensile)
+        //list.remove()
+        arrow.classList.toggle('elementTri__arrow--rotate')
+        dropdown.classList.remove('elementTri__dropdown--active')
       })
     });
     
   })
 
-  // rotate de la flèche et ajout de la dropdown (avec les éléments li)
-  arrow.addEventListener('click', () => {
-    arrow.classList.toggle('elementTri__arrow--rotate')
-    dropdown.classList.toggle('elementTri__dropdown--active')
-  })
+  
 }
