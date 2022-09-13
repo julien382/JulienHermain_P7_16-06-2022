182451b3c2822f50629e7500516f2aad107e548f
julien382 <julhermain@gmail.com>
2022-09-14 00:01:42 +0200

    message d'ereur ok

--- scripts/app/utils/searchMain_Recipies.js
+++ scripts/app/utils/searchMain_Recipies.js
@@ -3,18 +3,22 @@ import { getLocalStorage, setLocalStorage } from "./localStorage.js";
 export const searchMain = (inputValue) => {
     const allRecipies = getLocalStorage();
     
+    const toto = document.getElementById("messError")
+    
     if (inputValue.length >= 3){
         const cards = document.querySelectorAll(".cardRecette")
-        
-        console.log("cards");
+
         cards.forEach(card => {
             const text = card.innerText.toLowerCase()
             const id = card.id
             
+            // si valeur est dans le texte
             if (text.includes(inputValue)){
                 allRecipies.forEach(recette => {
                     if (recette.id == id){
                         recette.display = true
+                        toto.style = "display: none";
+                        console.log(toto);
                     }
                 })
             }
@@ -22,17 +26,22 @@ export const searchMain = (inputValue) => {
                 allRecipies.forEach(recette => {
                     if (recette.id == id){
                         recette.display = false
+                        toto.style = "display: block";
+                        console.log(toto);
                     }
                 })
             }
-            console.log(text);
         }) 
-
+        
     }
     else{
         allRecipies.forEach(recette => {
             recette.display = true
+            toto.style = "display: none";
+            console.log(toto);
         })
     }
     setLocalStorage(allRecipies)
+    
+
 }
