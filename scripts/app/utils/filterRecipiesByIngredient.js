c6c9c9df8778f468c891f94e801bf4e1433e84cb
julien382 <julhermain@gmail.com>
2022-09-30 13:30:00 +0200

    save 2 versions ok

--- scripts/app/utils/filterRecipiesByIngredient.js
+++ scripts/app/utils/filterRecipiesByIngredient.js
@@ -6,7 +6,7 @@ export const filterRecipiesByIngredient = (ingredient) => {
     //const allIngredients = getAllIngredients(allRecipies); // ['pomme', 'poire', 'fraise']
 
     // 2 - boucler sur les allRecettes
-    /*allRecipies.forEach(recipe => {
+    allRecipies.forEach(recipe => {
       // 3 - voir si dans chaque recettes l'ingrédient est présent dans la liste d'ingrédients
       if (recipe.display == true){
         const ingredientRecette = recipe.ingredients.map(element => element.ingredient.toLowerCase())
@@ -20,8 +20,8 @@ export const filterRecipiesByIngredient = (ingredient) => {
 
     })
     setLocalStorage(allRecipies)
-    */
-    for (let i = 0; i < allRecipies.length; i++) {
+    
+    /*for (let i = 0; i < allRecipies.length; i++) {
       const recipe = allRecipies[i];
       
       if (recipe.display == true){
@@ -35,7 +35,7 @@ export const filterRecipiesByIngredient = (ingredient) => {
       }
     }
 
-    setLocalStorage(allRecipies)
+    setLocalStorage(allRecipies)*/
       
     // indexOf()
     // includes()
